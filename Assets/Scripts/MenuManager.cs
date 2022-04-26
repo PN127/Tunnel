84eea3b132976d535ee46f128483ee9c23e484c6
PN127 <pasch.makhonin20109646@gmail.com>
2022-04-26 23:30:42 +0300

    закончен первый день

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text _versionText;
+    [SerializeField]
+    private string _version;
+
+    private void Start()
+    {
+        _versionText.text = $"ver: {_version}";
+    }
+
     public void LoadScene(int sceneid)
     {
         SceneManager.LoadScene(sceneid);
     }
+
+    public void Exit()
+    {
+        UnityEditor.EditorApplication.isPaused = true;
+    }
+
+
 }
